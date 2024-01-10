3ad18b22c474467eaeba8db2e33b04940e7f5951
Franklin <franklin.op@hotmail.com>
2024-01-10 11:32:22 +0100

    added verify token from tiktok

--- tiktok.py
+++ tiktok.py
@@ -5,6 +5,6 @@ from TikTokApi import TikTokApi as tiktok
 import json  
 
 #Get cookie data - INCOMPLETE
-verifyFp = ""
+verifyFp = "verify_lr7n1c0e_hzfMeVrw_iRgb_4Qv0_AQ48_hNqXKPPLMPfo"
 #Setup instance
 api = tiktok.get_instance(custom_verifyFp=verifyFp, use_test_endpoinds= True)
